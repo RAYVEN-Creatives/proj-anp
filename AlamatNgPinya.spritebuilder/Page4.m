e4259e05932593f24fae1f2b3e5b346fae768ebb
rocalworks <krisha@rayvencreatives.com>
2015-01-29 17:56:14 +0800

    change colour of burn effect (going page5 na!)

--- AlamatNgPinya.spritebuilder/Page4.m
+++ AlamatNgPinya.spritebuilder/Page4.m
@@ -10,4 +10,34 @@
 
 @implementation Page4
 
+{
+    CCNode *_burnEffectNode;
+}
+
+- (void) didLoadFromCCB {
+    // enable touch input
+    self.userInteractionEnabled = TRUE;
+    
+    // show current page
+    CCLOG(@"Page 4 loaded.");
+}
+
+- (void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    [self burnEffect];
+}
+
+- (void) burnEffect {
+    // remove all children
+    [_burnEffectNode removeAllChildren];
+    
+    // load sprite to
+    CCSprite *be = (CCSprite*) [CCBReader load:@"assets/ccbFiles/BurnEffect"];
+
+    // add effect to child
+    [_burnEffectNode addChild:be];
+    
+    // play burn effect
+    [be.animationManager runAnimationsForSequenceNamed:@"Touch Pot"];
+}
+
 @end
