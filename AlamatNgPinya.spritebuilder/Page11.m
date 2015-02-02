f24d2e55ab52ba0b75fb9f6ad4fa5e4ce0895feb
rocalworks <krisha@rayvencreatives.com>
2015-02-02 17:04:14 +0800

    + page10-12 on Xcode (still working on page 10)

---
+++ AlamatNgPinya.spritebuilder/Page11.m
@@ -0,0 +1,25 @@
+//
+//  Page11.m
+//  AlamatNgPinya
+//
+//  Created by Ray Venenoso on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Page11.h"
+
+@implementation Page11
+
+{
+    
+}
+
+- (void) didLoadFromCCB {
+    // enable touch inputs
+    self.userInteractionEnabled = TRUE;
+    
+    // notice
+    CCLOG(@"Page 11 loaded.");
+}
+
+@end
---
+++ AlamatNgPinya.spritebuilder/Page12.h
@@ -0,0 +1,13 @@
+//
+//  Page12.h
+//  AlamatNgPinya
+//
+//  Created by Ray Venenoso on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Page12 : CCNode
+
+@end
