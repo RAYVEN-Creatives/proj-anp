d9356fbea40862b6f9c10116facf9b02cc9ab35a
rocalworks <krisha@rayvencreatives.com>
2015-02-02 13:33:19 +0800

    + page 6 & 7 :3

---
+++ AlamatNgPinya.spritebuilder/Page6.h
@@ -0,0 +1,13 @@
+//
+//  Page6.h
+//  AlamatNgPinya
+//
+//  Created by Ray Venenoso on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Page6 : CCNode
+
+@end
---
+++ AlamatNgPinya.spritebuilder/Page7.m
@@ -0,0 +1,47 @@
+//
+//  Page7.m
+//  AlamatNgPinya
+//
+//  Created by Ray Venenoso on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Page7.h"
+
+@implementation Page7
+
+{
+    CCNode *_leftEyeNode;
+    CCNode *_rightEyeNode;
+    CCSprite *_pinangEyesNode;
+    BOOL firstClick;
+}
+
+- (void) didLoadFromCCB {
+    // enable touch input
+    self.userInteractionEnabled = TRUE;
+    
+    // load page...
+    CCLOG(@"Page 7 loaded.");
+    
+    // disable first click (on pinang's eyes)
+    firstClick = FALSE;
+}
+
+// BUG: Need to disable click while Pinang is still crying
+- (void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // get touch location
+    CGPoint touchLocation = [touch locationInWorld];
+    
+    // if user touches pinang's eyes...
+    if (CGRectContainsPoint([_leftEyeNode boundingBox], touchLocation) || CGRectContainsPoint([_rightEyeNode boundingBox], touchLocation)) {
+        
+        // play Crying
+        if (firstClick) {
+            [_pinangEyesNode. animationManager runAnimationsForSequenceNamed:@"Crying"];
+            firstClick = FALSE;
+        } else [_pinangEyesNode. animationManager runAnimationsForSequenceNamed:@"Continue Crying"];
+    }
+}
+
+@end
